04a9525a55ff4b693ecdf02a0a53a6bb6eb0a065
gofman <eid@freeton-virtualbox.localdomain>
2021-02-18 18:22:46 +0200

    Fix build deps

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -2,10 +2,12 @@
 
 echo "INFO: install dependencies..."
 
-echo "deb [arch=amd64] https://packages.confluent.io/deb/5.5 stable main" >> /etc/apt/sources.list
+wget http://security.ubuntu.com/ubuntu/pool/main/o/openssl1.0/libssl1.0.0_1.0.2n-1ubuntu5.6_amd64.deb
 
 apt-get update && apt-get install -y \
+    ./libssl1.0.0_1.0.2n-1ubuntu5.6_amd64.deb \
     librdkafka1 \
+    librdkafka++1 \
     librdkafka-dev \
     unzip \
     curl \
@@ -23,6 +25,10 @@ apt-get update && apt-get install -y \
     clang \
     git \
     jq
+
+echo "deb [arch=amd64] https://packages.confluent.io/deb/6.1 stable main" >> /etc/apt/sources.list
+apt-get update && apt-get install -y \
+    librdkafka-dev
 
 # This script downloads latest version but 1.45.2 was tested by TONLabs
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y 
